19dadbc176991ff2035824166cb8c356927a36bb
krzysztof.drozdowski <krzysztof.drozdowski@angrynerds.pl>
2021-03-18 10:39:02 +0100

    dev

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const path = require('path');
 
 const app = express();
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-//
-// const pageDir = path.join(__dirname, 'page');
-// app.use(express.static(path.join(__dirname, 'page')));
-// app.use(express.static(path.join(__dirname, 'images')));
-// app.use(express.static(path.join(__dirname, 'weights')));
-// app.use(express.static(path.join(__dirname, 'node_modules/face-api.js/dist')));
-// app.use(express.static(path.join(__dirname, 'node_modules/stackblur-canvas/dist')));
-//
-// app.get('/', (req) => res.sendFile(pageDir));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const pageDir = path.join(__dirname, 'page');
+app.use(express.static(path.join(__dirname, 'page')));
+app.use(express.static(path.join(__dirname, 'images')));
+app.use(express.static(path.join(__dirname, 'weights')));
+app.use(express.static(path.join(__dirname, 'node_modules/face-api.js/dist')));
+app.use(express.static(path.join(__dirname, 'node_modules/stackblur-canvas/dist')));
+
+app.get('/', (req) => res.sendFile(pageDir));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port: ${port}`));
